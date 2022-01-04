b7bd84862ff32f2333faa88a99073007b2052e36
sans-connaissance <davemalicke@gmail.com>
2022-01-03 21:45:37 -0500

    Initial Commit

---
+++ ResumeApp/ResumeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeAppApp.swift
+//  ResumeApp
+//
+//  Created by David Malicke on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ResumeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
