d2ce19327c60eb74a3096877b10f1c50a9e57a1b
sans-connaissance <davemalicke@gmail.com>
2022-01-04 18:53:23 -0500

    ADD initial vars and setup funcs to HomeVM

--- ResumeApp/Home/HomeVM.swift
+++ ResumeApp/Home/HomeVM.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class HomeVM: ObservableObject {
+    
+    // String == Category
+    @Published var resumeItems: [String: [ResumeItem]] = [:]
+    
+    //This pulls the keys from the movies dictionary and returns an array of strings
+    public var allCategories: [String] {
+        resumeItems.keys.map({ String($0) })
+    }
+    
+    init() {
+        setupResumeItems()
+    }
+    
+    public func getResumeItem(forCat cat: String) -> [ResumeItem] {
+        resumeItems[cat] ?? []
+    }
+    
+    
+    func setupResumeItems() {
+        var educationArray: [ResumeItem] = []
+        educationArray.append(exampleResumeItems[0])
+        educationArray.append(exampleResumeItems[1])
+        educationArray.append(exampleResumeItems[2])
+        resumeItems["Education"] = educationArray
+        resumeItems["Work Experience"] = exampleResumeItems.shuffled()
+        
+    }
+}
