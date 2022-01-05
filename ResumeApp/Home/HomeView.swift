0aa749df1ed668ac667649d1e4b483971dd8e327
sans-connaissance <davemalicke@gmail.com>
2022-01-04 19:01:34 -0500

    ADD initial HomeView UI

--- ResumeApp/Home/HomeView.swift
+++ ResumeApp/Home/HomeView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct HomeView: View {
+    var vm = HomeVM()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            //main VStack
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    ForEach(vm.allCategories, id: \.self) { category in
+                        VStack {
+                            HStack {
+                                Text(category)
+                                    .font(.title3)
+                                    .bold()
+                                Spacer()
+                            }
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                //This lazy hstack is optional
+                                LazyHStack{
+                                    ForEach(vm.getResumeItem(forCat: category)) { resumeItem in
+                                        StandardResumeItem(resumeItem: resumeItem)
+                                            .frame(width: 100, height: 200)
+                                            .padding(.horizontal, 20)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .foregroundColor(.white)
     }
 }
 
--- ResumeApp/ResumeAppApp.swift
+++ ResumeApp/ResumeAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ResumeAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
