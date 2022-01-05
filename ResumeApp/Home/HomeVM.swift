1ae759e488cfe5336a4c05ec7b7770eccda03efa
sans-connaissance <davemalicke@gmail.com>
2022-01-04 21:35:10 -0500

    ROLLBACK

--- ResumeApp/Home/HomeVM.swift
+++ ResumeApp/Home/HomeVM.swift
@@ -11,11 +11,11 @@ import AVFoundation
 class HomeVM: ObservableObject {
     
     // String == Category
-    @Published var resumeItems: [Category: [ResumeItem]] = [:]
+    @Published var resumeItems: [String: [ResumeItem]] = [:]
     
     //This pulls the keys from the movies dictionary and returns an array of strings
     public var allCategories: [String] {
-        resumeItems.keys.map({ String($0.rawValue) })
+        resumeItems.keys.map({ String($0) })
     }
     
     
@@ -25,7 +25,7 @@ class HomeVM: ObservableObject {
     }
     
     //returns an array of resumeItems by input Category
-    public func getResumeItem(forCat cat: Category) -> [ResumeItem] {
+    public func getResumeItem(forCat cat: String) -> [ResumeItem] {
         resumeItems[cat] ?? []
     }
     
@@ -35,8 +35,8 @@ class HomeVM: ObservableObject {
         educationArray.append(exampleResumeItems[0])
         educationArray.append(exampleResumeItems[1])
         educationArray.append(exampleResumeItems[2])
-        resumeItems[Category.Education] = educationArray
-        resumeItems[Category.Experience] = exampleResumeItems.shuffled()
+        resumeItems["Education"] = educationArray
+        resumeItems["Experience"] = exampleResumeItems.shuffled()
         
     }
 }
--- ResumeApp/Home/HomeView.swift
+++ ResumeApp/Home/HomeView.swift
@@ -7,56 +7,45 @@
 
 import SwiftUI
 
-struct HomeView: View {
-    var vm = HomeVM()
-    
-    var body: some View {
-        ZStack {
-            Color.black
-                .edgesIgnoringSafeArea(.all)
-            
-            //main VStack
-            ScrollView(showsIndicators: false) {
-                LazyVStack {
-                    CategoryView(categoryTitle: .Education, resumeItem: vm.getResumeItem(forCat: .Education), width: 100, height: 200)
-                    CategoryView(categoryTitle: .Experience, resumeItem: vm.getResumeItem(forCat: .Experience), width: 200, height: 400)
-                    
+
+    struct HomeView: View {
+        var vm = HomeVM()
+        
+        var body: some View {
+            ZStack {
+                Color.black
+                    .edgesIgnoringSafeArea(.all)
+                
+                //main VStack
+                ScrollView(showsIndicators: false) {
+                    LazyVStack {
+                        ForEach(vm.allCategories, id: \.self) { category in
+                            VStack {
+                                HStack {
+                                    Text(category)
+                                        .font(.title3)
+                                        .bold()
+                                    Spacer()
+                                }
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    //This lazy hstack is optional
+                                    LazyHStack{
+                                        ForEach(vm.getResumeItem(forCat: category)) { resumeItem in
+                                            StandardResumeItem(resumeItem: resumeItem)
+                                                .frame(width: 100, height: 200)
+                                                .padding(.horizontal, 20)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
+            .foregroundColor(.white)
         }
-        .foregroundColor(.white)
     }
-}
 
-
-struct CategoryView: View {
-    
-    let categoryTitle: Category
-    let resumeItem: [ResumeItem]
-    let width: CGFloat
-    let height: CGFloat
-    
-    var body: some View {
-        HStack {
-            Text(categoryTitle.rawValue)
-                .font(.title3)
-                .bold()
-            Spacer()
-        }
-        ScrollView(.horizontal, showsIndicators: false) {
-            //This lazy hstack is optional
-            LazyHStack{
-                ForEach(resumeItem) { resumeItem in
-                    StandardResumeItem(resumeItem: resumeItem)
-                        .frame(width: width, height: height)
-                        .padding(.horizontal, 25)
-                }
-            }
-        }
-        
-        
-    }
-}
 
 
 
