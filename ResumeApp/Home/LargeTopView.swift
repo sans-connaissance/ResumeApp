0144a4fd68299de52326fa180da080dee79e241d
sans-connaissance <davemalicke@gmail.com>
2022-01-07 20:17:41 -0500

    ADD WhiteButton

---
+++ ResumeApp/Home/LargeTopView.swift
@@ -0,0 +1,20 @@
+//
+//  LargeTopView.swift
+//  ResumeApp
+//
+//  Created by David Malicke on 1/7/22.
+//
+
+import SwiftUI
+
+struct LargeTopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LargeTopView_Previews: PreviewProvider {
+    static var previews: some View {
+        LargeTopView()
+    }
+}
---
+++ ResumeApp/Home/SmallVerticalButton.swift
@@ -0,0 +1,20 @@
+//
+//  SmallVerticalButton.swift
+//  ResumeApp
+//
+//  Created by David Malicke on 1/7/22.
+//
+
+import SwiftUI
+
+struct SmallVerticalButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SmallVerticalButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SmallVerticalButton()
+    }
+}
