5d3f8ba50b3c4d1add91c53683e2fe43d06c9ec2
sans-connaissance <davemalicke@gmail.com>
2022-01-07 20:13:53 -0500

    PREP for branch

--- ResumeApp/Home/HomeView.swift
+++ ResumeApp/Home/HomeView.swift
@@ -7,34 +7,32 @@
 
 import SwiftUI
 
+struct HomeView: View {
+    var vm = HomeVM()
 
-    struct HomeView: View {
-        var vm = HomeVM()
-        
-        var body: some View {
-            ZStack {
-                Color.black
-                    .edgesIgnoringSafeArea(.all)
-                
-                //main VStack
-                ScrollView(showsIndicators: false) {
-                    LazyVStack {
-                        ForEach(vm.allCategories, id: \.self) { category in
-                            VStack {
-                                HStack {
-                                    Text(category)
-                                        .font(.title3)
-                                        .bold()
-                                    Spacer()
-                                }
-                                ScrollView(.horizontal, showsIndicators: false) {
-                                    //This lazy hstack is optional
-                                    LazyHStack{
-                                        ForEach(vm.getResumeItem(forCat: category)) { resumeItem in
-                                            StandardResumeItem(resumeItem: resumeItem)
-                                                .frame(width: 100, height: 200)
-                                                .padding(.horizontal, 20)
-                                        }
+    var body: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+
+            // main VStack
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    ForEach(vm.allCategories, id: \.self) { category in
+                        VStack {
+                            HStack {
+                                Text(category)
+                                    .font(.title3)
+                                    .bold()
+                                Spacer()
+                            }
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                // This lazy hstack is optional
+                                LazyHStack {
+                                    ForEach(vm.getResumeItem(forCat: category)) { resumeItem in
+                                        StandardResumeItem(resumeItem: resumeItem)
+                                            .frame(width: 100, height: 200)
+                                            .padding(.horizontal, 20)
                                     }
                                 }
                             }
@@ -42,12 +40,10 @@ import SwiftUI
                     }
                 }
             }
-            .foregroundColor(.white)
         }
+        .foregroundColor(.white)
     }
-
-
-
+}
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
