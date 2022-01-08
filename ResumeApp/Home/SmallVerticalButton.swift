d470b38e0b530528f3a93e69860bc9cc35a69dae
sans-connaissance <davemalicke@gmail.com>
2022-01-07 20:19:34 -0500

    ADD SmallVerticalButton

--- ResumeApp/Home/SmallVerticalButton.swift
+++ ResumeApp/Home/SmallVerticalButton.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct SmallVerticalButton: View {
+    var text: String
+    var isOnImage: String
+    var isOffImage: String
+    var isOn: Bool
+    
+    var imageName: String {
+        if isOn {
+            return isOnImage
+        } else {
+            return isOffImage
+        }
+    }
+    
+    
+    
+    
+    var action: () -> Void
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            action()
+        } label: {
+            VStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.white)
+                Text(text)
+                    .foregroundColor(.white)
+                    .font(.system(size: 14))
+                    .bold()
+            }
+        }
     }
 }
 
 struct SmallVerticalButton_Previews: PreviewProvider {
     static var previews: some View {
-        SmallVerticalButton()
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            SmallVerticalButton(text: "My List",
+                                isOnImage: "checkmark",
+                                isOffImage: "plus",
+                                isOn: true) {
+                print("tapped")
+            }
+        }
     }
 }
