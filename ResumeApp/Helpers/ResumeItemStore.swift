de3bc01970fa817f9b1294bb24fe9fa39c047339
sans-connaissance <davemalicke@gmail.com>
2022-01-22 12:30:13 -0500

    ADD jsonResume schema based model and some associated data

--- ResumeApp/Helpers/ResumeItemStore.swift
+++ ResumeApp/Helpers/ResumeItemStore.swift
@@ -9,201 +9,209 @@ import Foundation
 import SwiftUI
 
 
-
-let frankfurtIndustries = ResumeItem(
+let davidMalicke = Resume(
     id: UUID().uuidString,
-    startDate: "February 2021",
-    endDate: "Present",
-    name: "Frankfurt Industries",
-    thumbnail: URL(string: "https://frankfurtindustries.neocities.org/images/bg.jpg")!,
-    location: "Ann Arbor, MI",
-    companyName: "Frankfurt Industries",
-    departmentName: nil,
-    jobTitle: "iOS Software Developer",
-    schoolName: nil,
-    degreeName: nil,
-    overLay: nil,
-    shortDescription: "Lead iOS software development at Frankfurt Industries.",
-    longDescription: nil,
-    list: ["Design, develop and publish UM SalaryPub on the App Store.", "UM SalaryPub is a native iOS app for searching, reviewing and comparing over 470,000 unique salary records.", "Built UM SalaryPub with SwiftUI following MVVM, and designed the CoreData model from the ground up. UM SalaryPub also takes advantage of an imported UIKit Charts package, which combined with SwiftUI and CoreData delivers a fast and responsive user experience."],
-    projects: [umSalaryPub],
-    categories: ["iOS, SwiftUI, CoreData"])
+    basics: resumeBasics,
+    volunteer: nil,
+    publications: [medAndSciencePub],
+    skills: <#T##[Resume.Skill]?#>,
+    references: <#T##[Resume.Reference]?#>,
+    work: <#T##[Resume.Work]?#>,
+    languages: <#T##[Resume.Language]?#>,
+    projects: <#T##[Resume.Project]?#>,
+    awards: <#T##[Resume.Award]?#>,
+    certificates: <#T##[Resume.Certificate]?#>,
+    education: <#T##[Resume.Education]?#>,
+    interests: <#T##[Resume.Interest]?#>,
+    videos: <#T##[Resume.Video]?#>)
 
-let operationsLead = ResumeItem(
+
+let resumeBasics = Resume.Basics(
+    location: location,
+    summary: "Experienced iOS Developer, Operations Lead, and Project Manager.",
+    profiles: [gitHub],
+    email: "davemalicke@gmail.com",
+    label: nil,
+    image: nil,
+    name: "David Malicke",
+    phone: "734-231-1111",
+    url: URL(string: "https://www.linkedin.com/in/david-malicke-ios/")!)
+
+let location = Resume.Basics.Location(
+    address: "1715 South BLVD",
+    postalCode: "48104",
+    city: "Ann Arbor",
+    region: "MI",
+    countryCode: "USA")
+
+let gitHub = Resume.Basics.Profile(
     id: UUID().uuidString,
-    startDate: "January 2017",
-    endDate: "Present",
-    name: "Operation's Lead",
-    thumbnail: URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/1899_Michigan_Wolverines_football_team.jpg/640px-1899_Michigan_Wolverines_football_team.jpg")!,
-    location: "Ann Arbor, MI",
-    companyName: "University of Michigan",
-    departmentName: "Center for Academic Innovation",
-    jobTitle: "Operation's Lead",
-    schoolName: nil,
-    degreeName: nil,
-    overLay: nil,
-    shortDescription: "The straw that stirs the drink.",
-    longDescription: "Areas of focus include internal communications, optimizing internal and external collaboration processes, and the development and maintenance of a variety of information processes.",
-    list: ["Work closely with CAI teams to develop and refine the underlying social, technical and process infrastructure required to meet the Center’s growing project and administrative needs.", "Improve project reporting and transparency by co-developing project management bandwidth boards and managing ingest of project portfolio data into Salesforce", "Operationalized cross-team processes for keeping Salesforce up-to-date", "Built and launched new intranet to serve as the Center’s internal platform for policies, guidelines, and HR resources","Co-developed copyright clearance process."],
-    projects: [salesForce],
-    categories: ["Change Management", "Process Management", "Mentorship"])
+    url: URL(string: "https://github.com/sans-connaissance"),
+    network: nil,
+    username: "sans-connaissance",
+    thumbnail: URL(string: "https://cdn-icons-png.flaticon.com/512/25/25231.png"))
 
-let projectManager = ResumeItem(
+let medAndSciencePub = Resume.Publication(
+        id: UUID().uuidString,
+        summary: "Chapter 2: Open Access Journal Publishing",
+        name: "Medical and Scientific Publishing: Author, Editor, and Reviewer Perspectives",
+        publisher: "Elsevier",
+        url: URL(string: "https://www.elsevier.com/books/medical-and-scientific-publishing/markovac/978-0-12-809969-8"),
+        releaseDate: "Nov, 10 2017",
+        thumbnail: URL(string: "https://secure-ecsd.elsevier.com/covers/80/Tango2/large/9780128099698.jpg"))
+
+let iosDev = Resume.Skill(
     id: UUID().uuidString,
-    startDate: "June 2015",
-    endDate: "January 2017",
-    name: "Project Manager",
-    thumbnail: URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/AngellHall2010.JPG/640px-AngellHall2010.JPG")!,
-    location: "Ann Arbor, MI",
-    companyName: "University of Michigan",
-    departmentName: "Center for Academic Innovation",
-    jobTitle: "Project Manager",
-    schoolName: nil,
-    degreeName: nil,
-    overLay: nil,
-    shortDescription: nil,
-    longDescription: "Managed the development, production, launch, and maintenance of 22 massive open online courses (MOOCs) and a variety of venture-funded digital education projects.",
-    list: ["Worked closely with faculty, learning experience designers, digital media specialists, and vendor partners throughout all course production processes", "Assembled and published course materials on vendor platforms."],
-    projects: [dataScienceEthics],
-    categories: ["Coursera", "Edx"])
-
-let masterDegree = ResumeItem(
-    id: UUID().uuidString,
-    startDate: nil,
-    endDate: "April 2011",
-    name: "Master of Science in Information",
-    thumbnail: URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Horace_H._Rackham_School_of_Graduate_Studies_%28March_2008%29.jpg/640px-Horace_H._Rackham_School_of_Graduate_Studies_%28March_2008%29.jpg")!,
-    location: "Ann Arbor, MI",
-    companyName: nil,
-    departmentName: nil,
-    jobTitle: nil,
-    schoolName: "University of Michigan",
-    //department
-    degreeName: "Master of Science in Information",
-    //specialization
-    overLay: nil,
-    //GPA
-    shortDescription: nil,
-    longDescription: nil,
-    list: ["Initiated into the Honor Society of Phi Kappa Phi. Membership is by invitation only to the top 10 percent of graduate students at a university"],
-    projects: nil,
-    categories: ["education"])
+    keywords: ["Agile", "CoreData", "GIT", "MVVM", "Vapor", "StackOverflow", "Swift",  "SwiftUI", "XCode"],
+    name: "iOS Developer Skills",
+    level: "Junior")
 
 
-//let bachelorDegree= ResumeItem(
+
+
+//
+//let frankfurtIndustries = ResumeItem(
+//    name: "Frankfurt Industries",
+//    startDate: "February 2021",
+//    endDate: "Present",
+//    location: "Ann Arbor, MI",
+//    job: iosDev,
+//    school: nil,
+//    video: nil,
+//    thumbnail: URL(string: "https://frankfurtindustries.neocities.org/images/bg.jpg")!,
+//    overLay: nil,
+//    shortDescription: "Lead iOS software development at Frankfurt Industries.",
+//    longDescription: nil,
+//    list: ["Design, develop and publish UM SalaryPub on the App Store.", "UM SalaryPub is a native iOS app for searching, reviewing and comparing over 470,000 unique salary records.", "Built UM SalaryPub with SwiftUI following MVVM, and designed the CoreData model from the ground up. UM SalaryPub also takes advantage of an imported UIKit Charts package, which combined with SwiftUI and CoreData delivers a fast and responsive user experience."],
+//    categories: ["iOS, SwiftUI, CoreData"],
+//    projects: [umSalaryPub])
+//
+//let iosDev = Job(
+//    employerName: "Frankfurt Industries",
+//    departmentName: nil,
+//    jobTitle: "iOS Software Developer")
+//
+//let umSalaryPub = Project(
+//    name: "UM SalaryPub",
+//    thumbnails: nil,
+//    videos: nil,
+//    link: nil,
+//    shortDescription: "An app for searching and comparing salaries",
+//    longDescription: "UM SalaryPub is a native iOS app for searching, reviewing and comparing over 470,000 unique salary records.",
+//    list: ["List of a bunch of things about the project", "Another list of things"],
+//    tags: ["iOS, SwiftUI, CoreData, UIKit"])
+//
+//
+//let frankfurtIndustries1 = ResumeItem(
 //    id: UUID().uuidString,
-//    startDate: <#T##String#>,
-//    endDate: <#T##String?#>,
-//    name: <#T##String#>,
-//    thumbnail: <#T##URL#>,
-//    location: <#T##String?#>,
-//    companyName: <#T##String?#>,
-//    departmentName: <#T##String?#>,
-//    jobTitle: <#T##String?#>,
-//    schoolName: <#T##String?#>,
-//    degreeName: <#T##String?#>,
-//    overLay: <#T##String?#>,
-//    shortDescription: <#T##String?#>,
-//    longDescription: <#T##String?#>,
-//    list: <#T##[String]?#>,
-//    projects: <#T##[Project]?#>,
-//    categories: <#T##[String]#>)
-
-
-//let sampleItem = ResumeItem(
-//    id: <#T##String#>,
-//    startDate: <#T##String#>,
-//    endDate: <#T##String?#>,
-//    name: <#T##String#>,
-//    thumbnail: <#T##URL#>,
-//    location: <#T##String?#>,
-//    companyName: <#T##String?#>,
-//    departmentName: <#T##String?#>,
-//    jobTitle: <#T##String?#>,
-//    schoolName: <#T##String?#>,
-//    degreeName: <#T##String?#>,
-//    overLay: <#T##String?#>,
-//    shortDescription: <#T##String?#>,
-//    longDescription: <#T##String?#>,
-//    list: <#T##[String]?#>,
-//    projects: <#T##[Project]?#>,
-//    categories: <#T##[String]#>)
-
-
-let pythonForEverybody = Project(
-    id: UUID().uuidString,
-    name: "Python for Everybody Course series",
-    thumbnails: [URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Charles_Severance.jpg/640px-Charles_Severance.jpg")!],
-    videos: nil,
-    link: URL(string: "https://www.coursera.org/specializations/python")!,
-    shortDescription: "One of the most successful series of online courses",
-    longDescription: "This thing made a lot of money, wow!",
-    list: ["The things I did for this course", "wow, i think it was a lot at the time."],
-    tags: ["Coursera", "MOOC", "Project Management"])
-
-let dataScienceEthics = Project(
-    id: UUID().uuidString,
-    name: "Data Science Ethics",
-    thumbnails: [URL(string: "https://upload.wikimedia.org/wikipedia/commons/d/dd/Amdahl_470V-6_computer_at_U_of_M.jpg")!],
-    videos: [URL(string: "https://www.youtube.com/watch?v=xYPSFc7ZL0U")!],
-    link: URL(string: "https://www.edx.org/course/data-science-ethics")!,
-    shortDescription: nil,
-    longDescription: nil,
-    list: nil,
-    tags: ["edx", "ethics", "data science"])
-
-
-let salesForce = Project (
-    id: UUID().uuidString,
-    name: "SalesForce",
-    thumbnails: nil,
-    videos: nil,
-    link: nil,
-    shortDescription: "Bending salesforce to our will.",
-    longDescription: "Salesforce as the insitutional repository of data at the Center of Academic Innovation.",
-    list: ["List of a bunch of things about the project", "Another list of things"],
-    tags: ["SalesForce","CRM", ""])
-
-let umSalaryPub = Project(
-    id: UUID().uuidString,
-    name: "UM SalaryPub",
-    thumbnails: nil,
-    videos: nil,
-    link: nil,
-    shortDescription: "An app for searching and comparing salaries",
-    longDescription: "UM SalaryPub is a native iOS app for searching, reviewing and comparing over 470,000 unique salary records.",
-    list: ["List of a bunch of things about the project", "Another list of things"],
-    tags: ["iOS, SwiftUI, CoreData, UIKit"])
-
-
-
+//    startDate: "February 2021",
+//    endDate: "Present",
+//    name: "Frankfurt Industries",
+//    thumbnail: URL(string: "https://frankfurtindustries.neocities.org/images/bg.jpg")!,
+//    location: "Ann Arbor, MI",
+//    companyName: "Frankfurt Industries",
+//    departmentName: nil,
+//    jobTitle: "iOS Software Developer",
+//    schoolName: nil,
+//    degreeName: nil,
+//    overLay: nil,
+//    shortDescription: "Lead iOS software development at Frankfurt Industries.",
+//    longDescription: nil,
+//    list: ["Design, develop and publish UM SalaryPub on the App Store.", "UM SalaryPub is a native iOS app for searching, reviewing and comparing over 470,000 unique salary records.", "Built UM SalaryPub with SwiftUI following MVVM, and designed the CoreData model from the ground up. UM SalaryPub also takes advantage of an imported UIKit Charts package, which combined with SwiftUI and CoreData delivers a fast and responsive user experience."],
+//    projects: [umSalaryPub],
+//    categories: ["iOS, SwiftUI, CoreData"])
+//
+//let operationsLead = ResumeItem(
+//    id: UUID().uuidString,
+//    startDate: "January 2017",
+//    endDate: "Present",
+//    name: "Operation's Lead",
+//    thumbnail: URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/1899_Michigan_Wolverines_football_team.jpg/640px-1899_Michigan_Wolverines_football_team.jpg")!,
+//    location: "Ann Arbor, MI",
+//    companyName: "University of Michigan",
+//    departmentName: "Center for Academic Innovation",
+//    jobTitle: "Operation's Lead",
+//    schoolName: nil,
+//    degreeName: nil,
+//    overLay: nil,
+//    shortDescription: "The straw that stirs the drink.",
+//    longDescription: "Areas of focus include internal communications, optimizing internal and external collaboration processes, and the development and maintenance of a variety of information processes.",
+//    list: ["Work closely with CAI teams to develop and refine the underlying social, technical and process infrastructure required to meet the Center’s growing project and administrative needs.", "Improve project reporting and transparency by co-developing project management bandwidth boards and managing ingest of project portfolio data into Salesforce", "Operationalized cross-team processes for keeping Salesforce up-to-date", "Built and launched new intranet to serve as the Center’s internal platform for policies, guidelines, and HR resources","Co-developed copyright clearance process."],
+//    projects: [salesForce],
+//    categories: ["Change Management", "Process Management", "Mentorship"])
+//
+//let projectManager = ResumeItem(
+//    id: UUID().uuidString,
+//    startDate: "June 2015",
+//    endDate: "January 2017",
+//    name: "Project Manager",
+//    thumbnail: URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/AngellHall2010.JPG/640px-AngellHall2010.JPG")!,
+//    location: "Ann Arbor, MI",
+//    companyName: "University of Michigan",
+//    departmentName: "Center for Academic Innovation",
+//    jobTitle: "Project Manager",
+//    schoolName: nil,
+//    degreeName: nil,
+//    overLay: nil,
+//    shortDescription: nil,
+//    longDescription: "Managed the development, production, launch, and maintenance of 22 massive open online courses (MOOCs) and a variety of venture-funded digital education projects.",
+//    list: ["Worked closely with faculty, learning experience designers, digital media specialists, and vendor partners throughout all course production processes", "Assembled and published course materials on vendor platforms."],
+//    projects: [dataScienceEthics],
+//    categories: ["Coursera", "Edx"])
+//
+//let masterDegree = ResumeItem(
+//    id: UUID().uuidString,
+//    startDate: nil,
+//    endDate: "April 2011",
+//    name: "Master of Science in Information",
+//    thumbnail: URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Horace_H._Rackham_School_of_Graduate_Studies_%28March_2008%29.jpg/640px-Horace_H._Rackham_School_of_Graduate_Studies_%28March_2008%29.jpg")!,
+//    location: "Ann Arbor, MI",
+//    companyName: nil,
+//    departmentName: nil,
+//    jobTitle: nil,
+//    schoolName: "University of Michigan",
+//    //department
+//    degreeName: "Master of Science in Information",
+//    //specialization
+//    overLay: nil,
+//    //GPA
+//    shortDescription: nil,
+//    longDescription: nil,
+//    list: ["Initiated into the Honor Society of Phi Kappa Phi. Membership is by invitation only to the top 10 percent of graduate students at a university"],
+//    projects: nil,
+//    categories: ["education"])
+//
 //let pythonForEverybody = Project(
-//    id: <#T##String#>,
-//    name: <#T##String#>,
-//    thumbnails: <#T##[URL]?#>,
-//    videos: <#T##[URL]?#>,
-//    link: <#T##URL?#>,
-//    shortDescription: <#T##String?#>,
-//    longDescription: <#T##String?#>,
-//    list: <#T##[String]?#>,
-//    tags: <#T##[String]?#>)
-
-
-//let sampleItem = ResumeItem(
-//    id: <#T##String#>,
-//    startDate: <#T##String#>,
-//    endDate: <#T##String?#>,
-//    name: <#T##String#>,
-//    thumbnail: <#T##URL#>,
-//    location: <#T##String?#>,
-//    companyName: <#T##String?#>,
-//    departmentName: <#T##String?#>,
-//    jobTitle: <#T##String?#>,
-//    schoolName: <#T##String?#>,
-//    degreeName: <#T##String?#>,
-//    overLay: <#T##String?#>,
-//    shortDescription: <#T##String?#>,
-//    longDescription: <#T##String?#>,
-//    list: <#T##[String]?#>,
-//    projects: <#T##[Project]?#>,
-//    categories: <#T##[String]#>)
+//    id: UUID().uuidString,
+//    name: "Python for Everybody Course series",
+//    thumbnails: [URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Charles_Severance.jpg/640px-Charles_Severance.jpg")!],
+//    videos: nil,
+//    link: URL(string: "https://www.coursera.org/specializations/python")!,
+//    shortDescription: "One of the most successful series of online courses",
+//    longDescription: "This thing made a lot of money, wow!",
+//    list: ["The things I did for this course", "wow, i think it was a lot at the time."],
+//    tags: ["Coursera", "MOOC", "Project Management"])
+//
+//let dataScienceEthics = Project(
+//    id: UUID().uuidString,
+//    name: "Data Science Ethics",
+//    thumbnails: [URL(string: "https://upload.wikimedia.org/wikipedia/commons/d/dd/Amdahl_470V-6_computer_at_U_of_M.jpg")!],
+//    videos: [URL(string: "https://www.youtube.com/watch?v=xYPSFc7ZL0U")!],
+//    link: URL(string: "https://www.edx.org/course/data-science-ethics")!,
+//    shortDescription: nil,
+//    longDescription: nil,
+//    list: nil,
+//    tags: ["edx", "ethics", "data science"])
+//
+//
+//let salesForce = Project (
+//    id: UUID().uuidString,
+//    name: "SalesForce",
+//    thumbnails: nil,
+//    videos: nil,
+//    link: nil,
+//    shortDescription: "Bending salesforce to our will.",
+//    longDescription: "Salesforce as the insitutional repository of data at the Center of Academic Innovation.",
+//    list: ["List of a bunch of things about the project", "Another list of things"],
+//    tags: ["SalesForce","CRM", ""])
+//
--- ResumeApp/Models/ResumeItem.swift
+++ ResumeApp/Models/ResumeItem.swift
@@ -7,73 +7,184 @@
 
 import Foundation
 
-//I NEED TO CHANGE THIS SO THAT THE RESUME DATA COMES PACKAGED IN A LARGER PERSON OBJECT
-//WITH THE REST OF THIS STUFF NESTED INSIDE IT.
-//Create mac app that allows you to publish data to the server.
-//Education should be an array of school objects
-//Experience should be an array of job/experience objects
+///QUESTIONS:
+///How do I pass URLs from server to app. Schema calls for String, but I must add them as URL(String)
 
-struct ResumeItem: Identifiable {
-    var id: String = UUID().uuidString
-    var name: String
-    var startDate: String?
-    var endDate: String?
-    var location: String?
-
-    var employer: Employer?
-    var school: School?
+struct Resume: Codable, Identifiable {
     
-    var thumbnail: URL
-    var video: Video?
-    var overLay: String?
-    var shortDescription: String?
-    var longDescription: String?
+    let id: String
     
-    var list: [String]?
-    var categories: [String]?
-    var projects: [Project]?
+    struct Basics: Codable {
+        struct Location: Codable {
+            let address: String?
+            let postalCode: String?
+            let city: String?
+            let region: String?
+            let countryCode: String?
+        }
+
+        let location: Location?
+
+        let summary: String?
+
+        struct Profile: Codable, Identifiable {
+            let id: String
+            let url: URL?
+            let network: String?
+            let username: String?
+            let thumbnail: URL?
+        }
+
+        let profiles: [Profile]?
+
+        let email: String?
+        let label: String?
+        let image: URL?
+        let name: String?
+        let phone: String?
+        let url: URL?
+    }
+
+    let basics: Basics?
+
+    struct Volunteer: Codable, Identifiable {
+        let id: String?
+        let summary: String?
+        let position: String?
+        let organization: String?
+        let startDate: String?
+        let endDate: String?
+        let highlights: [String]?
+        let url: URL?
+    }
+
+    let volunteer: [Volunteer]?
+
+    struct Publication: Codable, Identifiable {
+        let id: String?
+        let summary: String?
+        let name: String?
+        let publisher: String?
+        let url: URL?
+        let releaseDate: String?
+        let thumbnail: URL?
+    }
+
+    let publications: [Publication]?
+
+    struct Skill: Codable, Identifiable {
+        let id: String?
+        let keywords: [String]?
+        let name: String?
+        let level: String?
+    }
+
+    let skills: [Skill]?
+
+    struct Reference: Codable, Identifiable {
+        let id: String?
+        let name: String?
+        let reference: String?
+    }
+
+    let references: [Reference]?
+
+    struct Work: Codable, Identifiable {
+        let id: String?
+        let summary: String?
+        let name: String?
+        let department: String?
+        let position: String?
+        let startDate: String?
+        let endDate: String?
+        let highlights: [String]?
+        let url: URL?
+        let thumbnail: URL?
+    }
+
+    let work: [Work]?
+
+    struct Language: Codable, Identifiable {
+        let id: String?
+        let fluency: String?
+        let language: String?
+    }
+
+    let languages: [Language]?
+
+    struct Project: Codable, Identifiable {
+        let id: String?
+        let name: String?
+        let startDate: String?
+        let roles: [String]?
+        let endDate: String?
+        let description: String?
+        let keywords: [String]?
+        let type: String?
+        let entity: String?
+        let url: URL?
+        let highlights: [String]?
+        let thumbnails: [URL]?
+        let videos: [Video]?
+    }
+
+    let projects: [Project]?
+
+    struct Award: Codable, Identifiable {
+        let id: String?
+        let summary: String?
+        let awarder: String?
+        let title: String?
+        let date: String?
+        let thumbnail: URL?
+    }
+
+    let awards: [Award]?
+
+    struct Certificate: Codable, Identifiable {
+        let id: String?
+        let name: String?
+        let issuer: String?
+        let date: String?
+        let url: URL?
+        let thumbnail: URL?
+    }
+
+    let certificates: [Certificate]?
+
+    struct Education: Codable, Identifiable {
+        let id: String?
+        let institution: String?
+        let schoolName: String?
+        let degreeName: String?
+        let specialization: String?
+        let studyType: String?
+        let startDate: String?
+        let score: String?
+        let endDate: String?
+        let courses: [String]?
+        let area: String?
+        let url: URL?
+        let image: URL?
+    }
+
+    let education: [Education]?
+
+    struct Interest: Codable, Identifiable {
+        let id: String?
+        let name: String?
+        let keywords: [String]?
+        let thumnail: URL?
+    }
+
+    let interests: [Interest]?
+
+    struct Video: Codable, Identifiable {
+        let id: String
+        let name: String
+        let videoURL: URL
+        let thumbnailImageURL: URL
+    }
+
+    let videos: [Video]?
 }
-
-struct Employer: Identifiable {
-    var id: String = UUID().uuidString
-    var employerName: String
-    var departmentName: String?
-    var jobTitle: String?
-}
-
-struct School: Identifiable {
-    var id: String = UUID().uuidString
-    var schoolName: String
-    var schoolName2: String?
-    var degreeName: String?
-    var specialization: String?
-    var gpa: Int
-}
-
-struct Project: Identifiable {
-    var id: String = UUID().uuidString
-    var name: String
-    var thumbnails: [URL]?
-    var videos: [Video]?
-    var link: URL?
-    var shortDescription: String?
-    var longDescription: String?
-    var list: [String]?
-    var tags: [String]?
-}
-
-struct Video: Identifiable {
-    
-    var id: String = UUID().uuidString
-    var name: String
-    var videoURL: URL
-    var thumbnailImageURL: URL
-}
-
-
-// VIDEOS NEEDS TO BE ITS OWN OBJECT
-
-
-
-
-
